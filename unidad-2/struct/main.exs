98da091cbb8336d9ae80cf72b94bad60ccf5a7db
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2025-09-30 15:32:05 -0500

    Update main.exs

--- unidad-2/struct/main.exs
+++ unidad-2/struct/main.exs
@@ -1,5 +1,15 @@
 defmodule Main do
   def main do
+    #Creación de un struct
+    u1 = %User{name: "Alberto", pass: "123"}
+    IO.inspect(u1)
+
+    #Acceso de la información de un struct
+    IO.puts(u1.name)
+
+    #Modificación de un struct
+    u2 = %User{u1 | pass: "456"}
+    IO.inspect(u2)
   end
 end
 
